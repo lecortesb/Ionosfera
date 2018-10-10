e19be5efcb1626cdae7aa2c2ba55f7008028edcd
Luis <lecortesb@gmail.com>
2018-10-10 15:48:03 -0500

    Lee todo un TEC MAP pero aun no lo guarda en una matriz.

--- src/tec/TEC.java
+++ src/tec/TEC.java
@@ -17,27 +17,33 @@ public class TEC {
      */
     public static void main(String[] args) {
         String cadena,sub;
-        int datos[],posEs,i;
+        int datos[],posEs,i,j;
+        double lat=87.5;
         datos=new int[73];
         try{
             BufferedReader lector=new BufferedReader(new FileReader("D:\\datos\\045\\jplg0450.14i\\datos.14i"));
             do{
-                do{
-                    cadena=lector.readLine();
-                    //System.out.println(""+cadena);
-                }while(cadena != null && cadena.contains("START OF TEC MAP") == false);
                 cadena=lector.readLine();
-                if(cadena !=null){
-                    //System.out.println(""+cadena);
-                }
-                //Ya se quito el renglón de la fecha
-                cadena=lector.readLine();
-                if(cadena !=null){
-                    System.out.println(""+cadena);
-                }
-                //do{//Ya se quito el renglon de la latitud y la longitud
-                    cadena=lector.readLine();//datos
+                //System.out.println(""+cadena);
+            }while(cadena != null && cadena.contains("START OF TEC MAP") == false);
+            cadena=lector.readLine();
+            if(cadena !=null){
+                //System.out.println(""+cadena);
+            }
+            //Ya se quito el renglón de la fecha
+            cadena=lector.readLine();
+            if(cadena !=null){
+                //System.out.println(""+cadena);
+            }
+            //Ya se quito el primer renglon de la latitud y la longitud
+            j=0;
+            do{
+                System.out.println(""+cadena);
+                cadena=lector.readLine();//Primera linea de datos
+                while(cadena!=null&&cadena.contains("LAT")==false&&cadena.contains("END")==false){//Ya se quito el renglon de la latitud y la longitud
+                    //cadena=lector.readLine();//datos
                     if(cadena !=null){
+                        System.out.println(""+cadena);
                         i=0;
                         posEs=0;
                         while(cadena.charAt(0)==' '){//Quita los espacios del principio
@@ -49,7 +55,7 @@ public class TEC {
                             //System.out.println(""+posEs);
                             sub=cadena.substring(0,posEs);
                             datos[i]=Integer.parseInt(sub);
-                            System.out.println(""+datos[i]);
+                            //System.out.println(""+datos[i]);
                             cadena=cadena.substring(posEs, cadena.length());
                             while(cadena.charAt(0)==' '){//Quita los espacios del principio de la siguiente iteracion
                                 cadena=cadena.substring(1);
@@ -60,12 +66,16 @@ public class TEC {
                             cadena=cadena.substring(1);
                         }*/
                         datos[i]=Integer.parseInt(cadena);
-                        System.out.println(""+datos[i]);
+                        //System.out.println(""+datos[i]);
                         i++;
                     }
-                //}while(cadena!=null&&cadena.contains("LAT/LON1")==false);
-            }while(cadena!=null);
+                    cadena=lector.readLine();
+                }
+                lat=lat-2.5;
+                j++;
+            }while(cadena!=null&&cadena.contains("END")==false&&j<71);
             lector.close();
+            System.out.println("latitud final:"+lat);
         }
         catch(FileNotFoundException e){
             System.out.println("Archivo no encontrado");
