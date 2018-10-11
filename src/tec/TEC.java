520561751e5bcad94c09ca7d8668c19540f33daa
LASALLE <LASALLE@10.1.47.216>
2018-10-11 11:31:06 -0500

    Lee todo un Tec Map y lo coloca dentro de un arreglo de 71x73. El Tec Map corresponde a todas las latitudes y todas las lingitudes para una hora dada.

--- src/tec/TEC.java
+++ src/tec/TEC.java
@@ -17,11 +17,18 @@ public class TEC {
      */
     public static void main(String[] args) {
         String cadena,sub;
-        int datos[],posEs,i,j;
+        int datos[][],posEs,i,j;
         double lat=87.5;
-        datos=new int[73];
+        datos=new int[71][73];
+        int año,mes,dia,hr,min,seg;
+        año=2014;
+        mes=2;
+        dia=14;
+        hr=0;
+        min=0;
+        seg=0;
         try{
-            BufferedReader lector=new BufferedReader(new FileReader("D:\\datos\\045\\jplg0450.14i\\datos.14i"));
+            BufferedReader lector=new BufferedReader(new FileReader("E:\\datos\\045\\jplg0450.14i\\datos.14i"));
             do{
                 cadena=lector.readLine();
                 //System.out.println(""+cadena);
@@ -36,15 +43,17 @@ public class TEC {
                 //System.out.println(""+cadena);
             }
             //Ya se quito el primer renglon de la latitud y la longitud
+            i=0;
             j=0;
             do{
                 System.out.println(""+cadena);
                 cadena=lector.readLine();//Primera linea de datos
+                i=0;
                 while(cadena!=null&&cadena.contains("LAT")==false&&cadena.contains("END")==false){//Ya se quito el renglon de la latitud y la longitud
                     //cadena=lector.readLine();//datos
                     if(cadena !=null){
-                        System.out.println(""+cadena);
-                        i=0;
+                        //System.out.println(""+cadena);
+                        //i=0;
                         posEs=0;
                         while(cadena.charAt(0)==' '){//Quita los espacios del principio
                             cadena=cadena.substring(1);
@@ -54,8 +63,8 @@ public class TEC {
                             posEs=cadena.indexOf(" ",0);
                             //System.out.println(""+posEs);
                             sub=cadena.substring(0,posEs);
-                            datos[i]=Integer.parseInt(sub);
-                            //System.out.println(""+datos[i]);
+                            datos[j][i]=Integer.parseInt(sub);
+                            System.out.print(";"+datos[j][i]);
                             cadena=cadena.substring(posEs, cadena.length());
                             while(cadena.charAt(0)==' '){//Quita los espacios del principio de la siguiente iteracion
                                 cadena=cadena.substring(1);
@@ -65,17 +74,26 @@ public class TEC {
                          /*while(cadena.charAt(0)==' '){
                             cadena=cadena.substring(1);
                         }*/
-                        datos[i]=Integer.parseInt(cadena);
-                        //System.out.println(""+datos[i]);
+                        datos[j][i]=Integer.parseInt(cadena);
+                        System.out.print(";"+datos[j][i]);
                         i++;
                     }
                     cadena=lector.readLine();
                 }
                 lat=lat-2.5;
                 j++;
+                System.out.println("*j="+(j-1)+"i="+i);
             }while(cadena!=null&&cadena.contains("END")==false&&j<71);
             lector.close();
             System.out.println("latitud final:"+lat);
+            
+            System.out.println("Inicio del arreglo");
+            for(j=0;j<71;j++){
+                System.out.println("");
+                for(i=0;i<73;i++){
+                    System.out.print(","+datos[j][i]);
+                }
+            }
         }
         catch(FileNotFoundException e){
             System.out.println("Archivo no encontrado");
